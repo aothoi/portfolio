36045f6ae8e98207211000e40efe53c22f98e39c
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-01-08 17:16:01 +0600

    home section

---
+++ src/home/Home.js
@@ -0,0 +1,17 @@
+import "./Home.css"
+
+export const Home = () => {
+  return (
+    <div className='homeMain'>
+        <p className="intro1">Hi, I'm Mehzabin</p>
+        <p className="intro2">I build web applications</p>
+        <p className="description">I am a software developer based in Dhaka, Bangladesh.
+            My goal is to always build elegant, scalable, and
+            reliable applications.
+            <br/> I am also enthusiastic 
+            about learning and adapting to the latest 
+            technology stacks and frameworks
+        </p>
+    </div>
+  )
+}
