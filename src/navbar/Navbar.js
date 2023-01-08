ceefb0a5b71ce4fd923b3747f089508ee42255ad
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-01-08 17:15:18 +0600

    navbar

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+export const Navbar = () => {
+  return (
+    <div className='navMain'>
+      <nav>
+        <a href='#about'>01. About</a>
+        <a href='#work'>02. Work</a>
+        <a href='#skill'>03. Skill</a>
+        <a href='#contact'>04. Contact</a>
+      </nav>
+    </div>
+  )
+}
